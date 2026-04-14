60ba4b1a785ee53df1fc3a884a47789c15d3abc4
Muhammad Ibrahim Zarar <119736250+mibrahimzarar@users.noreply.github.com>
2026-04-14 17:43:04 +0500

    Converted to Vercel

---
+++ api/app-redirect.js
@@ -0,0 +1,24 @@
+module.exports = (req, res) => {
+  const userAgent = req.headers["user-agent"] || "";
+  const host = req.headers.host || "sunnah-way-app.vercel.app";
+  const protocol = req.headers["x-forwarded-proto"] || "https";
+  const websiteUrl = `${protocol}://${host}`;
+
+  const config = {
+    androidAppId: "com.sunnahway.app",
+    iosAppId: "6761305443",
+    websiteUrl,
+  };
+
+  let redirectUrl = config.websiteUrl;
+
+  if (userAgent.toLowerCase().includes("android")) {
+    redirectUrl = `https://play.google.com/store/apps/details?id=${config.androidAppId}`;
+  } else if (/iPhone|iPad|iPod/i.test(userAgent)) {
+    redirectUrl = `https://apps.apple.com/us/app/sunnah-way/id${config.iosAppId}`;
+  }
+
+  res.setHeader("Cache-Control", "no-cache");
+  res.writeHead(302, { Location: redirectUrl });
+  res.end();
+};
